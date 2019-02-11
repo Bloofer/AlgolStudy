f6af34ff744a2c5cb81fae6fd3ea681c515ecb78
jmyang <jmyang@ropas.snu.ac.kr>
2019-02-11 20:52:28 +0900

    add day10

---
+++ day10/parenthesis.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+bool valid;
+int cnt1, cnt2, cnt3, cnt4;
+string chars;
+
+void start_alg(){
+
+}
+
+void read_file(string fileName){
+    ifstream iFile(fileName.c_str());
+
+    int charCnt;
+    iFile >> charCnt;
+    for(int i=0; i<charCnt; i++) {
+        
+    }
+}
+
+int main(){
+
+    return 0;
+}
