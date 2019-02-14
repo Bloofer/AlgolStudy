30d783c12e2f99ca1507687bac1360a807b52666
jmyang <jmyang@ropas.snu.ac.kr>
2019-02-14 18:43:02 +0900

    add day10

--- day10/parenthesis.cpp
+++ day10/parenthesis.cpp
@@ -3,25 +3,34 @@
 #include <fstream>
 using namespace std;
 
-bool valid;
-int cnt1, cnt2, cnt3, cnt4;
+int cnt1, cnt2, cnt3, cnt4; // (), [], {}, <> 짝 갯수
 string chars;
+int charCnt;
 
-void start_alg(){
-
+bool start_alg(){
+    cnt1 = cnt2 = cnt3 = cnt4 = 0;
+    for(int i=0; i<charCnt; i++){
+        if(chars.at(i) == '(') cnt1++;
+        else if(chars.at(i) == '[') cnt2++;
+        else if(chars.at(i) == '{') cnt3++;
+        else if(chars.at(i) == '<') cnt4++;
+        else if(chars.at(i) == ')') cnt1--;
+        else if(chars.at(i) == ']') cnt2--;
+        else if(chars.at(i) == '}') cnt3--;
+        else if(chars.at(i) == '>') cnt4--;
+    }
+    return !(cnt1 || cnt2 || cnt3 || cnt4); // 괄호 짝 갯수가 맞지않으면 0, 맞으면 1
 }
 
-void read_file(string fileName){
-    ifstream iFile(fileName.c_str());
-
-    int charCnt;
-    iFile >> charCnt;
-    for(int i=0; i<charCnt; i++) {
-        
+void read_file(){
+    for(int i=0; i<10; i++){
+        cin >> charCnt;
+        cin >> chars;
+        cout << "#" << i+1 << " " << start_alg() << endl;
     }
 }
 
-int main(){
-
+int main(int argc, char** argv){
+    read_file();
     return 0;
 }
