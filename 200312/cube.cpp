89197c68b7d17a2b2f9d1166346e5f72484011b0
jmyang <jmyang@ropas.snu.ac.kr>
2020-03-13 00:13:19 +0900

    refactor cube

--- 200312/cube.cpp
+++ 200312/cube.cpp
@@ -15,19 +15,31 @@ void init() {
 	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[R][i][j] = 'b';
 }
 
+void turn_side_cw(side s) {
+	char tmp2d[3][3] = {
+		{ cube[s][2][0], cube[s][1][0], cube[s][0][0] },
+		{ cube[s][2][1], cube[s][1][1], cube[s][0][1] },
+		{ cube[s][2][2], cube[s][1][2], cube[s][0][2] }
+	};
+	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[s][i][j] = tmp2d[i][j]; 
+}
+
+void turn_side_ccw(side s) {
+	char tmp2d[3][3] = {
+		{ cube[s][0][2], cube[s][1][2], cube[s][2][2] },
+		{ cube[s][0][1], cube[s][1][1], cube[s][2][1] },
+		{ cube[s][0][0], cube[s][1][0], cube[s][2][0] }
+	};
+	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[s][i][j] = tmp2d[i][j];\
+}
+
 void turn_up_cw() {
 	char tmp[3] = { cube[F][0][0], cube[F][0][1], cube[F][0][2] };
 	for (int i = 0; i < 3; i++) cube[F][0][i] = cube[R][0][i]; // 오른 윗줄 → 앞 윗줄
 	for (int i = 0; i < 3; i++) cube[R][0][i] = cube[B][0][i]; // 뒤 윗줄 → 오른 윗줄
 	for (int i = 0; i < 3; i++) cube[B][0][i] = cube[L][0][i]; // 왼 윗줄 → 뒤 윗줄
 	for (int i = 0; i < 3; i++) cube[L][0][i] = tmp[i]; // 앞 윗줄 → 왼 윗줄
-
-	char tmp2d[3][3] = {
-		{ cube[U][2][0], cube[U][1][0], cube[U][0][0] },
-		{ cube[U][2][1], cube[U][1][1], cube[U][0][1] },
-		{ cube[U][2][2], cube[U][1][2], cube[U][0][2] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[U][i][j] = tmp2d[i][j]; // 윗면 시계방향 회전
+	turn_side_cw(U);
 }
 
 void turn_up_ccw() {
@@ -36,13 +48,7 @@ void turn_up_ccw() {
 	for (int i = 0; i < 3; i++) cube[L][0][i] = cube[B][0][i]; // 뒤 윗줄 → 왼 윗줄
 	for (int i = 0; i < 3; i++) cube[B][0][i] = cube[R][0][i]; // 오른 윗줄 → 뒤 윗줄
 	for (int i = 0; i < 3; i++) cube[R][0][i] = tmp[i]; // 앞 윗줄 → 오른 윗줄
-
-	char tmp2d[3][3] = {
-		{ cube[U][0][2], cube[U][1][2], cube[U][2][2] },
-		{ cube[U][0][1], cube[U][1][1], cube[U][2][1] },
-		{ cube[U][0][0], cube[U][1][0], cube[U][2][0] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[U][i][j] = tmp2d[i][j]; // 윗면 반시계방향 회전
+	turn_side_ccw(U);
 }
 
 void turn_down_cw() {
@@ -51,13 +57,7 @@ void turn_down_cw() {
 	for (int i = 0; i < 3; i++) cube[L][2][i] = cube[B][2][i]; // 뒤 윗줄 → 왼 윗줄
 	for (int i = 0; i < 3; i++) cube[B][2][i] = cube[R][2][i]; // 오른 윗줄 → 뒤 윗줄
 	for (int i = 0; i < 3; i++) cube[R][2][i] = tmp[i]; // 앞 윗줄 → 오른 윗줄
-
-	char tmp2d[3][3] = {
-		{ cube[D][2][0], cube[D][1][0], cube[D][0][0] },
-		{ cube[D][2][1], cube[D][1][1], cube[D][0][1] },
-		{ cube[D][2][2], cube[D][1][2], cube[D][0][2] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[D][i][j] = tmp2d[i][j]; // 아랫면 시계방향 회전
+	turn_side_cw(D);
 }
 
 void turn_down_ccw() {
@@ -66,13 +66,7 @@ void turn_down_ccw() {
 	for (int i = 0; i < 3; i++) cube[R][2][i] = cube[B][2][i]; // 뒤 아랫줄 → 오른 아랫줄
 	for (int i = 0; i < 3; i++) cube[B][2][i] = cube[L][2][i]; // 왼 아랫줄 → 뒤 아랫줄
 	for (int i = 0; i < 3; i++) cube[L][2][i] = tmp[i]; // 앞 아랫줄 → 왼 아랫줄
-
-	char tmp2d[3][3] = {
-		{ cube[D][0][2], cube[D][1][2], cube[D][2][2] },
-		{ cube[D][0][1], cube[D][1][1], cube[D][2][1] },
-		{ cube[D][0][0], cube[D][1][0], cube[D][2][0] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[D][i][j] = tmp2d[i][j]; // 아랫면 반시계방향 회전
+	turn_side_ccw(D);
 }
 
 void turn_front_cw() {
@@ -81,13 +75,7 @@ void turn_front_cw() {
 	for (int i = 0; i < 3; i++) cube[U][2][i] = cube[L][2-i][2]; // 위 아랫줄 → 왼 오른줄
 	for (int i = 0; i < 3; i++) cube[L][2-i][2] = cube[D][0][2-i]; // 왼 오른줄 → 아래 윗줄
 	for (int i = 0; i < 3; i++) cube[D][0][2-i] = tmp[i]; // 아래 윗줄 → 오른 왼줄
-
-	char tmp2d[3][3] = {
-		{ cube[F][2][0], cube[F][1][0], cube[F][0][0] },
-		{ cube[F][2][1], cube[F][1][1], cube[F][0][1] },
-		{ cube[F][2][2], cube[F][1][2], cube[F][0][2] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[F][i][j] = tmp2d[i][j]; // 앞면 시계방향 회전
+	turn_side_cw(F);
 }
 
 void turn_front_ccw() {
@@ -96,13 +84,7 @@ void turn_front_ccw() {
 	for (int i = 0; i < 3; i++) cube[D][0][2-i] = cube[L][2-i][2]; // 아래 윗줄 → 왼 오른줄
 	for (int i = 0; i < 3; i++) cube[L][2-i][2] = cube[U][2][i]; // 왼 오른줄 → 위 아랫줄
 	for (int i = 0; i < 3; i++) cube[U][2][i] = tmp[i]; // 위 아랫줄 → 오른 왼줄
-
-	char tmp2d[3][3] = {
-		{ cube[F][0][2], cube[F][1][2], cube[F][2][2] },
-		{ cube[F][0][1], cube[F][1][1], cube[F][2][1] },
-		{ cube[F][0][0], cube[F][1][0], cube[F][2][0] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[F][i][j] = tmp2d[i][j]; // 앞면 반시계방향 회전
+	turn_side_ccw(F);
 }
 
 void turn_back_cw() {
@@ -111,13 +93,7 @@ void turn_back_cw() {
 	for (int i = 0; i < 3; i++) cube[D][2][2 - i] = cube[L][2 - i][0]; // 아래 아랫줄 → 왼 왼줄
 	for (int i = 0; i < 3; i++) cube[L][2 - i][0] = cube[U][0][i]; // 왼 왼줄 → 위 윗줄
 	for (int i = 0; i < 3; i++) cube[U][0][i] = tmp[i]; // 위 윗줄 → 오른 오른줄
-
-	char tmp2d[3][3] = {
-		{ cube[B][2][0], cube[B][1][0], cube[B][0][0] },
-		{ cube[B][2][1], cube[B][1][1], cube[B][0][1] },
-		{ cube[B][2][2], cube[B][1][2], cube[B][0][2] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[B][i][j] = tmp2d[i][j]; // 뒷면 시계방향 회전
+	turn_side_cw(B);
 }
 
 void turn_back_ccw() {
@@ -126,13 +102,7 @@ void turn_back_ccw() {
 	for (int i = 0; i < 3; i++) cube[U][0][i] = cube[L][2-i][0]; // 위 윗줄 → 왼 왼줄
 	for (int i = 0; i < 3; i++) cube[L][2-i][0] = cube[D][2][2-i]; // 왼 왼줄 → 아래 아랫줄
 	for (int i = 0; i < 3; i++) cube[D][2][2-i] = tmp[i]; // 아래 아랫줄 → 오른 오른줄
-
-	char tmp2d[3][3] = {
-		{ cube[B][0][2], cube[B][1][2], cube[B][2][2] },
-		{ cube[B][0][1], cube[B][1][1], cube[B][2][1] },
-		{ cube[B][0][0], cube[B][1][0], cube[B][2][0] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[B][i][j] = tmp2d[i][j]; // 뒷면 반시계방향 회전
+	turn_side_ccw(B);
 }
 
 void turn_right_cw() {
@@ -141,13 +111,7 @@ void turn_right_cw() {
 	for (int i = 0; i < 3; i++) cube[U][2-i][2] = cube[F][2-i][2]; // 위 오른줄 → 앞 오른줄
 	for (int i = 0; i < 3; i++) cube[F][2-i][2] = cube[D][2-i][2]; // 앞 오른줄 → 아래 오른줄
 	for (int i = 0; i < 3; i++) cube[D][2-i][2] = tmp[i]; // 아래 오른줄 → 뒤 왼줄
-
-	char tmp2d[3][3] = {
-		{ cube[R][2][0], cube[R][1][0], cube[R][0][0] },
-		{ cube[R][2][1], cube[R][1][1], cube[R][0][1] },
-		{ cube[R][2][2], cube[R][1][2], cube[R][0][2] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[R][i][j] = tmp2d[i][j]; // 오른면 시계방향 회전
+	turn_side_cw(R);
 }
 
 void turn_right_ccw() {
@@ -156,13 +120,7 @@ void turn_right_ccw() {
 	for (int i = 0; i < 3; i++) cube[D][2-i][2] = cube[F][2-i][2]; // 아래 오른줄 → 앞 오른줄
 	for (int i = 0; i < 3; i++) cube[F][2-i][2] = cube[U][2-i][2]; // 앞 오른줄 → 위 오른줄
 	for (int i = 0; i < 3; i++) cube[U][2-i][2] = tmp[i]; // 위 오른줄 → 뒤 왼줄
-
-	char tmp2d[3][3] = {
-		{ cube[R][0][2], cube[R][1][2], cube[R][2][2] },
-		{ cube[R][0][1], cube[R][1][1], cube[R][2][1] },
-		{ cube[R][0][0], cube[R][1][0], cube[R][2][0] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[R][i][j] = tmp2d[i][j]; // 오른면 반시계방향 회전
+	turn_side_ccw(R);
 }
 
 void turn_left_cw() {
@@ -171,13 +129,7 @@ void turn_left_cw() {
 	for (int i = 0; i < 3; i++) cube[D][2 - i][0] = cube[F][2 - i][0]; // 아래 왼줄 → 앞 왼줄
 	for (int i = 0; i < 3; i++) cube[F][2 - i][0] = cube[U][2 - i][0]; // 앞 왼줄 → 위 왼줄
 	for (int i = 0; i < 3; i++) cube[U][2 - i][0] = tmp[i]; // 위 왼줄 → 뒤 오른줄
-
-	char tmp2d[3][3] = {
-		{ cube[L][2][0], cube[L][1][0], cube[L][0][0] },
-		{ cube[L][2][1], cube[L][1][1], cube[L][0][1] },
-		{ cube[L][2][2], cube[L][1][2], cube[L][0][2] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[L][i][j] = tmp2d[i][j]; // 왼면 시계방향 회전
+	turn_side_cw(L);
 }
 
 void turn_left_ccw() {
@@ -186,13 +138,7 @@ void turn_left_ccw() {
 	for (int i = 0; i < 3; i++) cube[U][2-i][0] = cube[F][2-i][0]; // 위 왼줄 → 앞 왼줄
 	for (int i = 0; i < 3; i++) cube[F][2-i][0] = cube[D][2-i][0]; // 앞 왼줄 → 아래 왼줄
 	for (int i = 0; i < 3; i++) cube[D][2-i][0] = tmp[i]; // 아래 왼줄 → 뒤 오른줄
-
-	char tmp2d[3][3] = {
-		{ cube[L][0][2], cube[L][1][2], cube[L][2][2] },
-		{ cube[L][0][1], cube[L][1][1], cube[L][2][1] },
-		{ cube[L][0][0], cube[L][1][0], cube[L][2][0] }
-	};
-	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) cube[L][i][j] = tmp2d[i][j]; // 왼면 반시계방향 회전
+	turn_side_ccw(L);
 }
 
 int main() {
